0da6399f6bd72459aed3e1409309d2fabd129aad
Alex Branham <branham@utexas.edu>
2016-10-17 13:41:54 -0500

    Attempt to get answers

--- get-quiz-info.R
+++ get-quiz-info.R
@@ -14,12 +14,13 @@ all_answers <- list()
 for (i in 1:length(data$assessment$section)) {
   item <- data$assessment$section[[i]]
   questions[i] <- item$presentation$material$mattext$text
-  ## answers <- list()
-  ## for (j in 1:length(item$presentation$response_lid$render_choice)) {
-  ##   render_choice <- item$presentation$response_lid[[j]]
-  ##   answers[j] <- render_choice$response_label$material$mattext$text
-  ##   all_answers <- answers
-  ## }
+  answers <- list()
+  for (j in 1:length(item$presentation$response_lid$render_choice)) {
+    response_label <- item$presentation$response_lid$render_choice[[j]]
+    answers[j] <- response_label$material$mattext$text
+    if (j == length(item$presentation$response_lid$render_choice)) {
+      ans_vec <- unlist(answers)
+      all_answers[i] <- ans_vec
+    }
+  }
 }
-
-unlist(questions)
