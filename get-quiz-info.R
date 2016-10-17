4d38276ad471b1a5c6f0d5ccb904474d237e5375
Alex Branham <branham@utexas.edu>
2016-10-17 13:43:41 -0500

    Adds some comments

--- get-quiz-info.R
+++ get-quiz-info.R
@@ -1,5 +1,7 @@
 library(XML)
 
+## For now, let's assume only multiple choice questions
+## quiz1.xml should be an exported quiz from Canvas.
 data <- xmlParse("quiz1.xml")
 
 data <- xmlToList(data)
@@ -12,13 +14,16 @@ data <- xmlToList(data)
 questions <- list()
 all_answers <- list()
 for (i in 1:length(data$assessment$section)) {
+  ## For each question, get the question text:
   item <- data$assessment$section[[i]]
   questions[i] <- item$presentation$material$mattext$text
   answers <- list()
   for (j in 1:length(item$presentation$response_lid$render_choice)) {
+    ## For all answers, get the answer text
     response_label <- item$presentation$response_lid$render_choice[[j]]
     answers[j] <- response_label$material$mattext$text
     if (j == length(item$presentation$response_lid$render_choice)) {
+      ## Put the answers in a list
       ans_vec <- unlist(answers)
       all_answers[i] <- ans_vec
     }
