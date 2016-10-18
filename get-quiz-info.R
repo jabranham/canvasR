8f25237730d5b5d98adcf592427d09000be336c3
Alex Branham <branham@utexas.edu>
2016-10-17 19:39:11 -0500

    Fixes style
    Gotta choose one style, I use this one:
    https://github.com/jimhester/lintr, based on Hadley's

--- get-quiz-info.R
+++ get-quiz-info.R
@@ -16,13 +16,13 @@ all_answers <- list()
 for (i in 1:length(data$assessment$section)) {
   ## For each question, get the question text:
   item <- data$assessment$section[[i]]
-  if(class(item) != "character"){
+  if (class(item) != "character"){
     questions[i] <- item$presentation$material$mattext$text
     answers <- list()
     for (j in 1:length(item$presentation$response_lid$render_choice)) {
       ## For all answers, get the answer text
       response_label <- item$presentation$response_lid$render_choice[[j]]
-      if(class(response_label$material$mattext) != "character"){
+      if (class(response_label$material$mattext) != "character"){
         answers[j] <- response_label$material$mattext$text
       }
       if (j == length(item$presentation$response_lid$render_choice)) {
@@ -35,16 +35,17 @@ for (i in 1:length(data$assessment$section)) {
 
 # in case not all questions have the same number of possible answers
 max_answers <- 0
-for(k in 1:length(all_answers)){
-  if(length(all_answers[[k]] > max_answers)){
+for (k in 1:length(all_answers)){
+  if (length(all_answers[[k]] > max_answers)){
     max_answers <- length(all_answers[[k]])
   }
 }
-for(l in 1:length(all_answers)){
-  if(length(all_answers[[l]] < max_answers)){
-    all_answers[[l]] <- c(all_answers[[l]], rep("", max_answers-length(all_answers[[l]])))
+for (l in 1:length(all_answers)){
+  if (length(all_answers[[l]] < max_answers)){
+    all_answers[[l]] <- c(all_answers[[l]],
+                         rep("", max_answers - length(all_answers[[l]])))
   }
-  if(is.null(all_answers[[l]])){
+  if (is.null(all_answers[[l]])){
     all_answers[[l]] <- rep("", max_answers)
   }
 }
@@ -55,6 +56,7 @@ output$question <- unlist(questions)
 
 # take care of html from questions or answers
 html_patterns <- c("<div>", "</div>", "<p>", "</p>")
-for(m in 1:length(html_patterns)){
-  output <- as.data.frame(apply(output, c(1,2), function(x) gsub(html_patterns[m], "", x)))
+for (m in 1:length(html_patterns)){
+  output <- as.data.frame(apply(output, c(1, 2),
+                               function(x) gsub(html_patterns[m], "", x)))
 }
