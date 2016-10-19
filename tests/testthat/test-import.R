8d91a7a8f030bae26323678dcc695d149223fffb
Alex Branham <branham@utexas.edu>
2016-10-19 09:20:39 -0500

    Updates test
    Since we're dropping answers that aren't true/false, the number of
    questions changed.

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,7 +1,8 @@
 library(canvasR)
+quiz <- system.file("extdata", "quiz.xml", package = "canvasR")
 
 test_that("Gets all questions", {
-  expect_equal(nrow(import_quiz(quiz)), 7)
+  expect_equal(nrow(import_quiz(quiz)), 4)
 })
 
 test_that("Gets all answers", {
